822d968c34385ac9f834039d97cec4a3205eb2e9
mssaang33 <mssaang33@gmail.com>
2023-05-11 06:03:17 +0900

    23/05/11 18:03

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+    base : '/blog/',
+    lang: 'utf-8',
+    title: 'MINLOG',
+    description: 'Just playing around'
+})
