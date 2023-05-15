fca92650955201f4763d3ea82195b7ff3e7d0c09
mssaang33 <mssaang33@gmail.com>
2023-05-15 17:48:49 +0900

    --all.sh master

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy with vuepress'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mssaang33/mssaang33.github.io.git master:gh-pages
+#git push -f git@github.com:mssaang33/mssaang33.github.io.git master:gh-pages
 
 cd -
